69f2a78a1294b6af148f1b492a5d78ec8f446f8a
Sebastien Mondet <seb@mondet.org>
2015-01-28 15:32:14 -0500

    Add a simpler pipeline to the demo-application
    Now pipelines are really chosen “by name.”

--- src/app/main.ml
+++ src/app/main.ml
@@ -8,7 +8,7 @@ let get_env s =
 let get_opt s =
   try Some (Sys.getenv s) with _ -> None
 
-let pipeline_example ~normal_fastqs ~tumor_fastqs ~dataset =
+let crazy_somatic_example ~normal_fastqs ~tumor_fastqs ~dataset =
   let open Biokepi_pipeline.Construct in
   let normal = input_fastq ~dataset normal_fastqs in
   let tumor = input_fastq ~dataset tumor_fastqs in
@@ -16,10 +16,12 @@ let pipeline_example ~normal_fastqs ~tumor_fastqs ~dataset =
     let normal =
       bwa ?gap_open_penalty ?gap_extension_penalty normal
       |> gatk_indel_realigner
+      |> picard_mark_duplicates
     in
     let tumor =
       bwa ?gap_open_penalty ?gap_extension_penalty tumor
       |> gatk_indel_realigner
+      |> picard_mark_duplicates
     in
     pair ~normal ~tumor in
   let bam_pairs = [
@@ -37,18 +39,49 @@ let pipeline_example ~normal_fastqs ~tumor_fastqs ~dataset =
   in
   vcfs
 
-let dump_dumb_pipeline_example () =
-  let dumb_fastq name =
-    Ketrew.EDSL.file_target (sprintf "/path/to/dump-%s.fastq.gz" name) ~name in
-  let normal_fastqs =
-    `Paired_end ([dumb_fastq "R1-L001"; dumb_fastq "R1-L002"],
-                 [dumb_fastq "R2-L001"; dumb_fastq "R2-L002"]) in
-  let tumor_fastqs =
-    `Single_end [dumb_fastq "R1-L001"; dumb_fastq "R1-L002"] in
-  let vcfs = pipeline_example  ~normal_fastqs ~tumor_fastqs ~dataset:"DUMB" in
-  say "Dumb examples dumped:\n%s"
-    (`List (List.map ~f:Biokepi_pipeline.to_json vcfs)
-     |> Yojson.Basic.pretty_to_string ~std:true)
+let simple_somatic_example ~variant_caller ~normal_fastqs ~tumor_fastqs ~dataset =
+  let open Biokepi_pipeline.Construct in
+  let normal = input_fastq ~dataset normal_fastqs in
+  let tumor = input_fastq ~dataset tumor_fastqs in
+  let make_bam data =
+    data |> bwa |> gatk_indel_realigner |> picard_mark_duplicates
+  in
+  let vc_input =
+    pair ~normal:(make_bam normal) ~tumor:(make_bam tumor) in
+  [variant_caller vc_input]
+
+type somatic_from_fastqs =
+  normal_fastqs:Biokepi_pipeline.Construct.input_fastq ->
+  tumor_fastqs:Biokepi_pipeline.Construct.input_fastq ->
+  dataset:string -> Biokepi_pipeline.vcf Biokepi_pipeline.t list
+
+type example_pipeline = [
+  | `Somatic_from_fastqs of somatic_from_fastqs
+]
+
+let global_named_examples: (string * example_pipeline) list = [
+  "somatic-crazy", `Somatic_from_fastqs crazy_somatic_example;
+  "somatic-simple-somaticsniper",
+  `Somatic_from_fastqs
+    (simple_somatic_example
+       ~variant_caller:Biokepi_pipeline.Construct.somaticsniper);
+]
+
+
+let dump_pipeline =
+  function
+  | `Somatic_from_fastqs pipeline_example ->
+    let dumb_fastq name =
+      Ketrew.EDSL.file_target (sprintf "/path/to/dump-%s.fastq.gz" name) ~name in
+    let normal_fastqs =
+      `Paired_end ([dumb_fastq "R1-L001"; dumb_fastq "R1-L002"],
+                   [dumb_fastq "R2-L001"; dumb_fastq "R2-L002"]) in
+    let tumor_fastqs =
+      `Single_end [dumb_fastq "R1-L001"; dumb_fastq "R1-L002"] in
+    let vcfs = pipeline_example  ~normal_fastqs ~tumor_fastqs ~dataset:"DUMB" in
+    say "Dumb examples dumped:\n%s"
+      (`List (List.map ~f:Biokepi_pipeline.to_json vcfs)
+       |> Yojson.Basic.pretty_to_string ~std:true)
 
 let environmental_box () : Biokepi_run_environment.Machine.t =
   let box_uri = get_env "BIOKEPI_SSH_BOX_URI" |> Uri.of_string in
@@ -73,63 +106,66 @@ let environmental_box () : Biokepi_run_environment.Machine.t =
   Biokepi_run_environment.Ssh_box.create
     ~gatk_jar_location ~mutect_jar_location ~meta_playground ssh_name
 
-let with_environmental_dataset make_pipe_line =
-  let dataset = get_env "BIOKEPI_DATASET_NAME" in
-  let get_list kind =
-    get_env (sprintf "BIOKEPI_%s" kind)
-    |> String.split ~on:(`Character ',')
-    |> List.map ~f:(String.strip ~on:`Both)
-    |> List.map ~f:(fun f ->
-        let name = sprintf "Input: %s (%s)" dataset kind in
-        Ketrew.EDSL.file_target f ~name)
-  in
-  let normal_fastqs =
-    `Paired_end (get_list "NORMAL_R1", get_list "NORMAL_R2") in
-  let tumor_fastqs =
-    `Paired_end (get_list "TUMOR_R1", get_list "TUMOR_R2") in
-  (dataset, make_pipe_line ~normal_fastqs ~tumor_fastqs ~dataset)
+let with_environmental_dataset =
+  function
+  | `Somatic_from_fastqs  make_pipe_line ->
+    let dataset = get_env "BIOKEPI_DATASET_NAME" in
+    let get_list kind =
+      get_env (sprintf "BIOKEPI_%s" kind)
+      |> String.split ~on:(`Character ',')
+      |> List.map ~f:(String.strip ~on:`Both)
+      |> List.map ~f:(fun f ->
+          let name = sprintf "Input: %s (%s)" dataset kind in
+          Ketrew.EDSL.file_target f ~name)
+    in
+    let normal_fastqs =
+      `Paired_end (get_list "NORMAL_R1", get_list "NORMAL_R2") in
+    let tumor_fastqs =
+      `Paired_end (get_list "TUMOR_R1", get_list "TUMOR_R2") in
+    (dataset, make_pipe_line ~normal_fastqs ~tumor_fastqs ~dataset)
 
-let dumb_pipeline_example_target ~push_result () =
-  let machine = environmental_box () in
-  let dataset, pipelines =
-    with_environmental_dataset pipeline_example in
-  let work_dir =
-    Biokepi_run_environment.Machine.work_dir machine
-    // sprintf "pipeline-dumb-on-%s" dataset in
-  let compiled =
-    List.map pipelines
-      ~f:(fun pl ->
-          let t =
-            Biokepi_pipeline.compile_variant_caller_step
-              ~work_dir ~machine pl in
-          `Target t,
-          `Json_blob (
-            `Assoc [
-              "target-name", `String t#name;
-              "target-id", `String t#id;
-              "pipeline", Biokepi_pipeline.to_json pl;
-            ]))
-  in
-  let dependencies =
-    List.map compiled (function
-      | (`Target vcf, `Json_blob json) when push_result ->
-        let witness_output =
-          Filename.chop_suffix vcf#product#path ".vcf" ^ "-cycledashed.html" in
-        let params = Yojson.Basic.pretty_to_string json in
-        Biokepi_target_library.Cycledash.post_vcf ~run_with:machine
-          ~vcf ~variant_caller_name:vcf#name ~dataset_name:dataset
-          ~witness_output ~params
-          (get_env "BIOKEPI_CYCLEDASH_URL")
-      | (`Target t, _) -> t
-      ) in
-  let whole_json =
-    `List (List.map compiled ~f:(fun (_, `Json_blob j) -> j)) in (*  *)
-  Ketrew.EDSL.target (sprintf "dumb on %s: common ancestor" dataset)
-    ~dependencies
-    ~metadata:(`String (Yojson.Basic.pretty_to_string whole_json))
+let pipeline_example_target ~push_result ~pipeline_name pipeline_example =
+    let machine = environmental_box () in
+    let dataset, pipelines =
+      with_environmental_dataset pipeline_example in
+    let work_dir =
+      Biokepi_run_environment.Machine.work_dir machine
+      // sprintf "pipeline-%s-on-%s" pipeline_name dataset in
+    let compiled =
+      List.map pipelines
+        ~f:(fun pl ->
+            let t =
+              Biokepi_pipeline.compile_variant_caller_step
+                ~work_dir ~machine pl in
+            `Target t,
+            `Json_blob (
+              `Assoc [
+                "target-name", `String t#name;
+                "target-id", `String t#id;
+                "pipeline", Biokepi_pipeline.to_json pl;
+              ]))
+    in
+    let dependencies =
+      List.map compiled (function
+        | (`Target vcf, `Json_blob json) when push_result ->
+          let witness_output =
+            Filename.chop_suffix vcf#product#path ".vcf" ^ "-cycledashed.html" in
+          let params = Yojson.Basic.pretty_to_string json in
+          Biokepi_target_library.Cycledash.post_vcf ~run_with:machine
+            ~vcf ~variant_caller_name:vcf#name ~dataset_name:dataset
+            ~witness_output ~params
+            (get_env "BIOKEPI_CYCLEDASH_URL")
+        | (`Target t, _) -> t
+        ) in
+    let whole_json =
+      `List (List.map compiled ~f:(fun (_, `Json_blob j) -> j)) in (*  *)
+    Ketrew.EDSL.target
+      (sprintf "%s on %s: common ancestor" pipeline_name dataset)
+      ~dependencies
+      ~metadata:(`String (Yojson.Basic.pretty_to_string whole_json))
 
-let run_dumb_pipeline_example ~push_result () =
-  let target = dumb_pipeline_example_target ~push_result () in
+let run_pipeline_example ~push_result ~pipeline_name pipeline =
+  let target = pipeline_example_target ~push_result ~pipeline_name pipeline in
   Ketrew.EDSL.run target
     
 let () =
@@ -140,7 +176,7 @@ let () =
      Term.info (fst info) ~version ~sdocs:"COMMON OPTIONS" ~doc:(snd info)) in
   let name_flag =
     Arg.(required & opt (some string) None & info ["N"; "name"]
-           ~doc:"Give a name to the pipeline") in
+           ~doc:"Choose the pipeline by “name”") in
   let push_to_cycledash_flag =
     Arg.(value & flag & info ["P"; "push-result"]
            ~doc:"Push the result to a running Cycledash.") in
@@ -149,9 +185,11 @@ let () =
       ~info:("dump-pipeline", "Dump the JSON blob of a pipeline")
       ~term:Term.(
           pure (fun name ->
-              match name with
-              | "dumb" -> dump_dumb_pipeline_example ()
-              | s -> failwithf "unknown pipeline: %S" s)
+              match
+                List.find global_named_examples ~f:(fun (n, _) -> n = name)
+              with
+              | Some (_, pipe) -> dump_pipeline pipe
+              | None -> failwithf "unknown pipeline: %S" name)
           $ name_flag
         )
   in
@@ -161,9 +199,12 @@ let () =
              "Run a pipeline the SSH-BOX defined by the environment")
       ~term:Term.(
           pure (fun name push_result ->
-              match name with
-              | "dumb" -> run_dumb_pipeline_example ~push_result ()
-              | s -> failwithf "unknown pipeline: %S" s)
+              match
+                List.find global_named_examples ~f:(fun (n, _) -> n = name)
+              with
+              | Some (_, pipe) ->
+                run_pipeline_example ~push_result ~pipeline_name:name pipe
+              | None -> failwithf "unknown pipeline: %S" name)
           $ name_flag
           $ push_to_cycledash_flag
         )
