00362c703864408a8bb1c09bd81f2c17cc2ab11a
Arun Ahuja <aahuja11@gmail.com>
2015-06-15 17:45:29 -0400

    add hisat rna aligner

---
+++ src/lib/hisat.ml
@@ -0,0 +1,105 @@
+open Common
+open Run_environment
+open Workflow_utilities
+
+let index
+  ~reference_build
+  ~processors
+  ~(run_with : Machine.t) =
+  let open KEDSL in
+  let reference_fasta =
+    Machine.get_reference_genome run_with reference_build
+      |> Reference_genome.fasta in
+  let hisat_tool = Machine.get_tool run_with Tool.Default.hisat in
+  let name =
+    sprintf "hisat-index-%s" (Filename.basename reference_fasta#product#path) in
+  let reference_dir = (Filename.dirname reference_fasta#product#path) in
+  let result_dir = sprintf "%s/hisat-index/" reference_dir in
+  let index_prefix = result_dir // "hisat-index" in
+  let first_index_file = sprintf "%s.1.bt2" index_prefix in
+  workflow_node ~name
+    (single_file ~host:(Machine.(as_host run_with)) first_index_file)
+    ~edges:[
+      on_failure_activate (Remove.directory ~run_with result_dir);
+      depends_on reference_fasta;
+      depends_on Tool.(ensure hisat_tool);
+    ]
+    ~tags:[Target_tags.aligner]
+    ~make:(Machine.run_program run_with ~processors ~name
+            Program.(
+              Tool.(init hisat_tool)
+              && shf "mkdir %s" result_dir 
+              && shf "hisat-build %s %s"
+                reference_fasta#product#path
+                index_prefix
+          ))
+
+let align 
+  ~reference_build
+  ~processors
+  ~fastq
+  ~(result_prefix:string)
+  ~(run_with : Machine.t)
+  () =
+  let open KEDSL in
+  let reference_fasta =
+    Machine.get_reference_genome run_with reference_build
+      |> Reference_genome.fasta in
+  let reference_dir = (Filename.dirname reference_fasta#product#path) in
+  let index_dir = sprintf "%s/hisat-index/" reference_dir in
+  let index_prefix = index_dir // "hisat-index" in
+  let in_work_dir =
+      Program.shf "cd %s" Filename.(quote (dirname result_prefix)) in
+  let hisat_tool = Machine.get_tool run_with Tool.Default.hisat in
+  let hisat_index = index ~reference_build ~run_with ~processors in
+  let reference_dir = (Filename.dirname reference_fasta#product#path) in
+  let hisat_index_dir = sprintf "%s/hisat-index/" reference_dir in
+  let result = sprintf "%s.sam" result_prefix in
+  let r1_path, r2_path_opt = fastq#product#paths in
+  let name = sprintf "hisat-rna-align-%s" (Filename.basename r1_path) in
+  let hisat_base_command = sprintf 
+               "hisat \
+                -p %d \
+                -x %s \
+                -S %s \
+                -1 %s"
+                processors
+                (Filename.quote index_prefix)
+                (Filename.quote result)
+                (Filename.quote r1_path)
+  in
+  let base_hisat_target ~hisat_command = 
+    workflow_node ~name
+      (bam_file 
+        ?sorting:(Some `Coordinate)
+        ?contains:(Some `RNA)
+        ~host:(Machine.(as_host run_with)) 
+        result)
+      ~edges:[
+            on_failure_activate (Remove.file ~run_with result);
+            depends_on reference_fasta;
+            depends_on hisat_index;
+            depends_on fastq;
+            depends_on Tool.(ensure hisat_tool);
+        ]
+        ~tags:[Target_tags.aligner]
+        ~make:(Machine.run_program run_with ~processors ~name
+             Program.(
+               Tool.(init hisat_tool)
+               && in_work_dir
+               && sh hisat_command 
+           ))
+  in
+  match r2_path_opt with
+  | Some read2 -> 
+    let hisat_command =
+      String.concat ~sep:" " [
+        hisat_base_command;
+        "-2";
+        (Filename.quote read2);
+      ] in
+      base_hisat_target ~hisat_command
+    | None -> 
+      let hisat_command = hisat_base_command in
+      base_hisat_target ~hisat_command
+
