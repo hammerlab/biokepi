0ae32558bd2d26f1744ce99975d97e2368518c98
Isaac Hodes <isaachodes@gmail.com>
2015-11-13 12:08:05 -0500

    Allow MuTect to take parameters

--- src/lib/mutect.ml
+++ src/lib/mutect.ml
@@ -2,9 +2,27 @@ open Common
 open Run_environment
 open Workflow_utilities
 
+module Configuration = struct
+
+  type t = { name: string; parameters: (string * string) list; }
+
+  let to_json {name; parameters}: Yojson.Basic.json =
+    `Assoc [
+      "name", `String name;
+      "parameters",
+      `Assoc (List.map parameters ~f:(fun (a, b) -> a, `String b));
+    ]
+  let render {parameters; _} =
+    List.concat_map parameters ~f:(fun (a,b) -> [a; b])
+
+  let default =
+    {name = "default"; parameters = []}
+
+end
+
 let run ?(reference_build=`B37)
     ~(run_with:Machine.t) ~normal ~tumor ~result_prefix
-    ?(more_edges = []) how =
+    ?(more_edges = []) ~configuration how =
   let open KEDSL in
   let run_on_region ~add_edges region =
     let result_file suffix =
@@ -35,44 +53,35 @@ let run ?(reference_build=`B37)
             Tool.(init mutect)
             && shf "mkdir -p %s" run_path
             && shf "cd %s" run_path
-            && shf "java -Xmx2g -jar $mutect_HOME/muTect-*.jar \
-                    --analysis_type MuTect \
-                    --reference_sequence %s \
-                    --cosmic %s \
-                    --dbsnp %s \
-                    %s \
-                    --input_file:normal %s \
-                    --input_file:tumor %s \
-                    --out %s \
-                    --vcf %s \
-                    --coverage_file %s "
-              fasta#product#path
-              cosmic#product#path
-              dbsnp#product#path
-              intervals_option
-              sorted_normal#product#path
-              sorted_tumor#product#path
-              dot_out_file
-              output_file
-              coverage_file
-          )
+            && sh ("java -Xmx2g -jar $mutect_HOME/muTect-*.jar --analysis_type MuTect " ^
+                     (String.concat ~sep:" " ([
+                       "--reference_sequence"; fasta#product#path;
+                       "--cosmic"; cosmic#product#path;
+                       "--dbsnp"; dbsnp#product#path;
+                       ""; intervals_option;
+                       "--input_file:normal"; sorted_normal#product#path;
+                       "--input_file:tumor"; sorted_tumor#product#path;
+                       "--out"; dot_out_file;
+                       "--vcf"; output_file;
+                       "--coverage_file"; coverage_file;
+                      ] @ Configuration.render configuration))))
       in
       workflow_node ~name ~make
         (single_file output_file ~host:Machine.(as_host run_with))
         ~tags:[Target_tags.variant_caller]
         ~edges:(add_edges @ [
-          depends_on Tool.(ensure mutect);
-          depends_on sorted_normal;
-          depends_on sorted_tumor;
-          depends_on fasta;
-          depends_on cosmic;
-          depends_on dbsnp;
-          depends_on fasta_dot_fai;
-          depends_on sequence_dict;
-          depends_on (Samtools.index_to_bai ~run_with sorted_normal);
-          depends_on (Samtools.index_to_bai ~run_with sorted_tumor);
-          on_failure_activate (Remove.file ~run_with output_file);
-        ])
+            depends_on Tool.(ensure mutect);
+            depends_on sorted_normal;
+            depends_on sorted_tumor;
+            depends_on fasta;
+            depends_on cosmic;
+            depends_on dbsnp;
+            depends_on fasta_dot_fai;
+            depends_on sequence_dict;
+            depends_on (Samtools.index_to_bai ~run_with sorted_normal);
+            depends_on (Samtools.index_to_bai ~run_with sorted_tumor);
+            on_failure_activate (Remove.file ~run_with output_file);
+          ])
     in
     run_mutect
   in
