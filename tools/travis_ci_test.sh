8bcab324f69992f4dcbfd5d12069bb3585ebd3e9
Sebastien Mondet <seb@mondet.org>
2016-01-28 18:19:59 -0500

    Fix travis build
    Now that the development version of Ketrew can build without database,
    we can remove all the annoying build hack to get Postgres and Sqlite.

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -2,7 +2,6 @@
 
 
 
-export opam_pin_add=""
 travis_install_on_linux () {
     # Install OCaml and OPAM PPAs
     case "$OCAML_VERSION,$OPAM_VERSION" in
@@ -12,29 +11,16 @@ travis_install_on_linux () {
         4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
         4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
         4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-        4.01.0,1.2.0) ppa=avsm/ocaml41+opam12; export opam_pin_add="add" ;;
+        4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
         4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-        4.02.0,1.2.0) ppa=avsm/ocaml42+opam12; export opam_pin_add="add" ;;
+        4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
         4.02.2,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-        4.02.2,1.2.0) ppa=avsm/ocaml42+opam12; export opam_pin_add="add" ;;
+        4.02.2,1.2.0) ppa=avsm/ocaml42+opam12 ;;
       *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
     esac
 
     echo "yes" | sudo add-apt-repository ppa:$ppa
     sudo apt-get update -qq
-
-    # Hack around Sqlite: https://github.com/cmu-is-projects/ferry/blob/master/.travis.yml
-    sudo apt-get autoremove sqlite3
-    sudo apt-add-repository -y ppa:travis-ci/sqlite3
-    sudo apt-get -y update
-    sudo apt-cache show sqlite3
-    sudo apt-get install sqlite3=3.7.15.1-1~travis1
-    sudo sqlite3 -version
-
-    sudo apt-get install libpq-dev postgresql-9.4
-
-    export PATH=$PATH:/usr/lib/postgresql/9.4/bin
-    echo "PATH: $PATH"
 
     export opam_init_options="--comp=$OCAML_VERSION"
     sudo apt-get install -qq  opam time git
@@ -47,12 +33,6 @@ travis_install_on_osx () {
 
     brew update
     brew install opam
-    brew install sqlite
-    # This path contains the version number which may change *often*
-    export PKG_CONFIG_PATH=`find /usr/local/Cellar/sqlite -depth 1 | tail -n 1`/lib/pkgconfig
-    echo "PKG_CONFIG_PATH: $PKG_CONFIG_PATH"
-    echo "/usr/local/Cellar/sqlite/:"
-    ls -l /usr/local/Cellar/sqlite/
     export opam_init_options="--comp=$OCAML_VERSION"
     export opam_pin_add="add"
 }
@@ -88,9 +68,6 @@ echo 'ocamlfind list | grep lwt'
 ocamlfind list | grep lwt
 echo 'ocamlfind list | grep cohttp'
 ocamlfind list | grep cohttp
-
-echo "Sqlite version"
-sqlite3 -version
 
 echo "Setting Warn-Error for the Travis test"
 export OCAMLPARAM="warn-error=A,_"
