c070fd136d1565996186ed8ddecc273d07d4588f
Leonid Rozenberg <leonidr@gmail.com>
2016-03-15 16:40:25 -0400

    Add biopam configuration, Picard demo.

--- src/lib/run_environment.ml
+++ src/lib/run_environment.ml
@@ -28,11 +28,14 @@ module Tool = struct
       [@@@ocaml.warning "-11"]
       type t = [
         | `Bwa of [ `V_0_7_10 ]
-        (* A tool that is manually installed by retrieving source or binary.*)
+        (* A tool that is installed by retrieving source or binary.*)
         | `Custom of string * string      
+        (* A tool that is installed via Biopam. *)
+        | `Biopamed of string
       ] [@@deriving yojson, show, eq]
     end
     let custom name ~version = `Custom (name, version)
+    let biopam name = `Biopamed name
   end
   module Default = struct
     open Definition
--- src/test/we_have_opam.ml
+++
@@ -1,8 +0,0 @@
-(* A workflow to make sure that we have opam. *)
-
-let destination = "/local/stable/rozenl02/"
-
-let () =
-  let machine = Biokepi.Build_machine.create destination in
-  let workflow = Biokepi.Opam.version machine in
-  Ketrew.Client.submit_workflow workflow
