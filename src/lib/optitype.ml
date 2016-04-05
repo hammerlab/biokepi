64498c9141307b30e5b8b73813b666a3ae5f8b62
Sebastien Mondet <seb@mondet.org>
2016-04-05 15:31:44 -0400

    Make Optitype work on single-end data

--- src/lib/optitype.ml
+++ src/lib/optitype.ml
@@ -8,10 +8,10 @@ open Run_environment
    Please provide a fresh [work_dir] directory, it will be deleted in case of
    failure.
 *)
-let hla_type ~work_dir ~run_with ~r1 ~r2 ~run_name nt =
+let hla_type ~work_dir ~run_with ~r1 ?r2 ~run_name nt =
   let tool = Machine.get_tool run_with (`Biopamed "optitype") in
   let r1pt = Filename.quote r1#product#path in
-  let r2pt = Filename.quote r2#product#path in
+  let r2pt_opt = Option.map ~f:(fun o -> Filename.quote o#product#path) r2 in
   let name = sprintf "optitype-%s" run_name in
   let make =
     Machine.run_big_program run_with ~name KEDSL.Program.(
@@ -25,7 +25,10 @@ let hla_type ~work_dir ~run_with ~r1 ~r2 ~run_name nt =
         sh "sed -i.bak \"s|\\/path\\/to\\/razers3|$(which razers3)|g\" config.ini"
         &&
         shf "OptiTypePipeline --verbose --input %s %s %s -o %s "
-          r1pt r2pt (match nt with | `DNA -> "--dna" | `RNA -> "--rna") run_name)
+          (Filename.quote r1pt)
+          (Option.value_map ~default:"" r2pt_opt ~f:Filename.quote)
+          (match nt with | `DNA -> "--dna" | `RNA -> "--rna")
+          run_name)
   in
   let product =
     let host = Machine.as_host run_with in
@@ -39,11 +42,13 @@ let hla_type ~work_dir ~run_with ~r1 ~r2 ~run_name nt =
     end
   in
   KEDSL.workflow_node product ~name ~make
-    ~edges:[
-      KEDSL.depends_on (Tool.ensure tool);
-      KEDSL.depends_on r1;
-      KEDSL.depends_on r2;
-      KEDSL.on_failure_activate
-        (Workflow_utilities.Remove.directory ~run_with work_dir);
-    ]
+    ~edges:(
+      Option.value_map ~default:[] r2 ~f:(fun w -> [KEDSL.depends_on w])
+      @ [
+        KEDSL.depends_on (Tool.ensure tool);
+        KEDSL.depends_on r1;
+        KEDSL.on_failure_activate
+          (Workflow_utilities.Remove.directory ~run_with work_dir);
+      ]
+    )
 
