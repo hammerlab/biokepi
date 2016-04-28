f832cbc11185d4cf0566422ee2e895432a3c7e20
Sebastien Mondet <seb@mondet.org>
2016-04-28 10:39:56 -0400

    Simplify the EDSL extension oexample

--- src/examples/edsl_extension_register_result.ml
+++ src/examples/edsl_extension_register_result.ml
@@ -77,29 +77,22 @@ module Example_pipeline (Registrable_bfx: Semantics_with_registration) = struct
       list_map fastq ~f:(lambda (fun fq ->
           bwa_mem fq ~reference_build:"b37"
           |> picard_mark_duplicates
-            ~configuration:Biokepi.Tools.Picard.Mark_duplicates_settings.default
         )) in
     let normal_bam = align normal |> merge_bams in
     let tumor_bam = align tumor |> merge_bams in
     let bam_pair = pair normal_bam tumor_bam in
-    let indel_realigned_pair =
-      gatk_indel_realigner_joint bam_pair
-        ~configuration: Biokepi.Tools.Gatk.Configuration.default_indel_realigner
-    in
+    let indel_realigned_pair = gatk_indel_realigner_joint bam_pair in
     let final_normal_bam =
       pair_first indel_realigned_pair
       |> gatk_bqsr
-        ~configuration: Biokepi.Tools.Gatk.Configuration.default_bqsr
       |> register "final-normal-bam"
     in
     let final_tumor_bam =
       pair_second indel_realigned_pair
       |> gatk_bqsr
-        ~configuration: Biokepi.Tools.Gatk.Configuration.default_bqsr
       |> register "final-normal-bam"
     in
     mutect ~normal:final_normal_bam ~tumor:final_tumor_bam
-      ~configuration:Biokepi.Tools.Mutect.Configuration.default
     |> register "mutect-vcf"
 
   let run ~normal ~tumor =
@@ -349,7 +342,7 @@ let tumor =
 (*M
 
 We submit the workflow to the Ketrew server (here using the `default`
-configuration):
+configuration profile):
 M*)
 let () =
   match Sys.argv.(1) with
