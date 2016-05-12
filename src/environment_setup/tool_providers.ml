174345561df85b5434ac794200d6f00f3e0bc550
B. Arman Aksoy <arman@aksoy.org>
2016-05-11 23:35:15 -0400

    add generalized, barebones support for Python tool installation

--- src/environment_setup/tool_providers.ml
+++ src/environment_setup/tool_providers.ml
@@ -448,5 +448,6 @@ let default_toolkit
     ];
     Biopam.default ~run_program ~host
       ~install_path:(install_tools_path // "biopam-kit") ();
+    Python_package.default ~host ~meta_playground ();
   ]
 
