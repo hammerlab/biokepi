d935ca4b56b9d5c7df633617e92725ebb4a9bf16
B. Arman Aksoy <arman@aksoy.org>
2016-07-08 16:04:45 -0400

    add mising .py extension to the isovar binary file

--- src/environment_setup/python_package.ml
+++ src/environment_setup/python_package.ml
@@ -62,7 +62,7 @@ let default ~host ~run_program ~install_path () =
     create_python_tool ~host ~run_program ~install_path 
       ~version:"0.1.2" (Pip, Package_PyPI "vcf-annotate-polyphen");
     create_python_tool ~host ~run_program ~install_path 
-      ~version:"0.1.3" ~check_bin:"isovar-protein-sequences"
+      ~version:"0.1.3" ~check_bin:"isovar-protein-sequences.py"
       (Pip, Package_PyPI "isovar");
     create_python_tool ~host ~run_program ~install_path 
       ~version:"0.0.21" (Pip, Package_PyPI "topiary");
